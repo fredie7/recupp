26ab32cda59fec6a63b94efb1380acce57c7f8d6
fredie7 <lexfrederick3@gmail.com>
2024-04-14 12:19:19 +0300

    nineth commit

--- src/App.js
+++ src/App.js
@@ -9,29 +9,37 @@ function App() {
       <div className="wrapper">
         <div className="container">
           <header className="header">
+            {/* <div class="box">
+              <div class="inner">
+                <span>RECUP</span>
+              </div>
+              <div class="inner">
+                <span>RECUP</span>
+              </div>
+            </div> */}
             <p className="be_seen">WANT TO BE SEEN AT THE CAMPUS ?</p>
             <div className="option_wrapper">
               <div className="options">
-                <div className="for">FOR:</div>
+                {/* <div className="for">FOR:</div> */}
                 <div className="ad_options">
                   <div className="ad_icon_wrapper">
                     <div className="ad_summer_job">
                       <div className="ad_icon">
                         <PiStudentLight size={27} />
                       </div>
-                      <p>summer jobs</p>
+                      <p className="recruit">recruitment</p>
                     </div>
                     <div className="ad_events">
                       <div className="ad_icon">
                         <VscSymbolEvent size={27} />
                       </div>
-                      <p>student events</p>
+                      <p className="stud_event">student events</p>
                     </div>
                     <div className="ad_summer_job">
                       <div className="ad_icon">
                         <MdEmojiEvents size={27} />
                       </div>
-                      <p>hackathons</p>
+                      <p className="hack">hackathons</p>
                     </div>
                   </div>
                 </div>
@@ -39,8 +47,10 @@ function App() {
             </div>
             <p className="reach_out">
               {/* You are one step away on to reaching out to your target audience - */}
-              Do fill in fill-in your contact details, make your request - Let's
-              make the magic happen!
+              {/* Do fill in fill-in your contact details, make your request - Let's
+              make the magic happen! */}
+              Sign up now to join our campaign or express your interest
+              in our service!
             </p>
           </header>
           <form action="" className="form">
@@ -50,7 +60,7 @@ function App() {
               placeholder="Enter company name"
             />
             <input
-              type="text"
+              type="email"
               className="company_email"
               placeholder="Enter company email"
             />
@@ -62,7 +72,7 @@ function App() {
               cols="30"
               rows="10"
             ></textarea>
-            <button className="btn">LAUNCH YOUR ADVERT</button>
+            <button className="btn">SUBMIT</button>
           </form>
         </div>
       </div>
